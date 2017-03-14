37b9b84218165bc7cd09a58d901ef40caa21355f
Brian Fujito <bfujito@gmail.com>
2017-03-14 15:52:56 -0400

    Release 0.1.2 - close down cluster connections

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+== Changes in Version 0.1.2 date 2017-02-14 ==
+
+Close down cluster connection in addition to session connection
+
 == Changes in Version 0.1.1 date 2017-02-22 ==
 
 We added the capability to save a data.frame to Cassandra. via `cass_save_df`
--- R/session.R
+++ R/session.R
@@ -7,18 +7,24 @@
 #' @param clusterName The name of the Cassandra cluster
 #' @param clusterKeyspace The name of the Cassandra keyspace in the cluster
 #' @export
-get_cass_session <- function(connectUrl, clusterName, clusterKeyspace) {
+get_cass_session <- function(connectUrl, clusterName, clusterKeyspace,debug=F) {
 
+  if (debug==T) { cat("get_cass_session:: ... setting classpath, java init, and loading jars\n")}
   jarBase <- system.file("java", package='cassandrasimple')
   rJava::.jinit()
   rJava::.jaddClassPath(Sys.glob(paste0(jarBase,"/*.jar")))
 
+  if (debug==T) { cat("get_cass_session:: ... initializing cluster\n")}
   jClusterBuilder = rJava::.jcall('com.datastax.driver.core.Cluster', "Lcom/datastax/driver/core/Cluster$Builder;", 'builder')
+  if (debug==T) { cat("get_cass_session:: ... setting cluster name\n")}
   jClusterBuilder = rJava::.jcall(jClusterBuilder,  "Lcom/datastax/driver/core/Cluster$Builder;", 'withClusterName', clusterName)
+  if (debug==T) { cat("get_cass_session:: ... adding contact point\n")}
   jClusterBuilder = rJava::.jcall(jClusterBuilder,  "Lcom/datastax/driver/core/Cluster$Builder;", 'addContactPoint', connectUrl)
+  if (debug==T) { cat("get_cass_session:: ... building cluster\n")}
   jCluster = rJava::.jcall(jClusterBuilder,  "Lcom/datastax/driver/core/Cluster;", 'build')
+  if (debug==T) { cat("get_cass_session:: ... getting session\n")}
   jCassSess = rJava::.jcall(jCluster, 'Lcom/datastax/driver/core/Session;', 'connect', clusterKeyspace)
-
+  if (debug==T) { cat("get_cass_session:: ... DONE\n")}
   return(jCassSess)
 }
 
@@ -26,7 +32,12 @@ get_cass_session <- function(connectUrl, clusterName, clusterKeyspace) {
 #'
 #' @param jCassSess Java com.datastax.driver.core.Session session, returned from get_cass_session
 #' @export
-close_cass_session <- function(jCassSess) {
+close_cass_session <- function(jCassSess,debug=F) {
+  if (debug==T) { cat("close_cass_session:: ... closing Cassandra session\n") }
+  jCluster = rJava::.jcall(jCassSess, "Lcom/datastax/driver/core/Cluster;", 'getCluster')
   rJava::.jcall(jCassSess, "V", 'close')
+  if (debug==T) { cat("close_cass_session:: ... closing Cassandra cluster\n") }
+  rJava::.jcall(jCluster, "V", 'close')
+  if (debug==T) { cat("close_cass_session:: ... closing complete for Cassandra close_cass_sess\n") }
   return(T)
 }
